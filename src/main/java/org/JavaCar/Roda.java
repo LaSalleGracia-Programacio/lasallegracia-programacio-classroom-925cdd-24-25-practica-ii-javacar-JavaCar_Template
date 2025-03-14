4638758ecdb56750b37a2149d18d6fe78d1a41c5
Gerard LaSalle <gerard.jalon@gracia.lasalle.cat>
2025-03-14 13:05:58 +0100

    Getters y constructor para Roda

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -3,4 +3,17 @@ package org.JavaCar;
 public class Roda {
     private String marca;
     private double diametre;
+
+    public Roda(String marca, double diametre) {
+        this.marca = marca;
+        this.diametre = diametre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getDiametre() {
+        return diametre;
+    }
 }
