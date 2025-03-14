9dd51f120c9ab2f53a18e190c2a92349d2ca9559
lluisp7 <luisperezutreras@gmail.com>
2025-03-14 12:38:12 +0100

    Update Vehicle.java

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,5 +1,21 @@
 package org.JavaCar;
 
-public abstract class  Vehicle {
-    
+public abstract class  Vehicle implements Llogable {
+    private String matricula;
+    private String marca;
+    private String model;
+    private double preuBase;
+    private double motor;
+    private double rodes;
+    private String  etiquetaAmbiental;
+
+    public Vehicle(String matricula, String marca, String model, double preuBase, double motor, double rodes, String etiquetaAmbiental) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+        this.etiquetaAmbiental = etiquetaAmbiental;
+    }
 }
