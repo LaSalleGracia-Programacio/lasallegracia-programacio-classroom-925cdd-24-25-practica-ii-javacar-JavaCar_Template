12282fc1025ea8514dfc086b3dbaf85096974f8c
lluisp7 <luisperezutreras@gmail.com>
2025-03-14 12:58:59 +0100

    moto java fet

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -1,4 +1,18 @@
 package org.JavaCar;
 
-public class Moto {
+public class Moto extends Vehicle {
+
+    public Moto(String matricula, String marca, String model, double preuBase, double motor, double rodes, String etiquetaAmbiental) {
+        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+    }
+    private double cilindrada;
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
 }
+
