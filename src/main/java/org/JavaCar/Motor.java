a8e5ce808de17140cc65d6beb32b4f4938cdd3b3
Gerard LaSalle <gerard.jalon@gracia.lasalle.cat>
2025-03-14 13:03:20 +0100

    Constructors y getters para Motor

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -3,4 +3,18 @@ package org.JavaCar;
 public class Motor {
     private String tipus;
     private int potencia;
+
+    public Motor(String tipus, int potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+    }
+
+    public String getTipus() {
+        return tipus;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
 }
+
