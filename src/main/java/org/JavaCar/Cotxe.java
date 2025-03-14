c106fe7e999cecdac18ad7598cb0001b96329c46
lluisp7 <luisperezutreras@gmail.com>
2025-03-14 13:49:59 +0100

    fix bug vehicles test

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -3,8 +3,9 @@ package org.JavaCar;
 public class Cotxe extends Vehicle implements Llogable {
     private int nombrePlaces;
 
-    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, int  motor, double rodes, String etiquetaAmbiental) {
-        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor  motor, Roda rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes);
+        this.nombrePlaces = nombrePlaces;
     }
 
     public int getNombrePlaces() {
--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -2,10 +2,11 @@ package org.JavaCar;
 
 public class Moto extends Vehicle {
 
-    public Moto(String matricula, String marca, String model, double preuBase, double cilindrada, double motor, double rodes, String etiquetaAmbiental) {
-        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
-    }
     private double cilindrada;
+
+    public Moto(String matricula, String marca, String model, double preuBase, double cilindrada, Motor motor, Roda rodes, String etiquetaAmbiental) {
+        super(matricula, marca, model, preuBase, motor, rodes);
+    }
 
     public double getCilindrada() {
         return cilindrada;
