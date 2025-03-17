a57ce1f7515efe40612ef18ff15acc74daeadf6d
Gerard LaSalle <gerard.jalon@gracia.lasalle.cat>
2025-03-17 09:25:03 +0100

    Fix orden constructor Furgoneta con capacitatCarga

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -4,13 +4,14 @@ public class Furgoneta extends Vehicle{
 
     private double capacitatCarga;
 
-    public Furgoneta(String matricula, String marca, String model, double preuBase, double motor, double rodes, String etiquetaAmbiental) {
+    public Furgoneta(String matricula, String marca, String model, double preuBase, double capacitatCarga, Motor motor, Roda rodes) {
         super(matricula, marca, model, preuBase, motor, rodes);
+        this.capacitatCarga = capacitatCarga;
     }
 
     @Override
     public double calcularPreu(int dies) {
-        if(capacitatCarga > 1000) {
+        if(capacitatCarga> 1000) {
             return super.preuLloguer(dies) + 10 * dies;
         } else {
             return super.preuLloguer(dies);
