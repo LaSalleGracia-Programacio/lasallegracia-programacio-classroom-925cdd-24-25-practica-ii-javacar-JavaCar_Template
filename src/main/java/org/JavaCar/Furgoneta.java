b88b6a3e185fe4df8bd193b39d65cb651133fd41
Gerard LaSalle <gerard.jalon@gracia.lasalle.cat>
2025-03-17 09:28:29 +0100

    Cambiar Roda por Roda[] para test en Furgoneta y Moto

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -4,7 +4,7 @@ public class Furgoneta extends Vehicle{
 
     private double capacitatCarga;
 
-    public Furgoneta(String matricula, String marca, String model, double preuBase, double capacitatCarga, Motor motor, Roda rodes) {
+    public Furgoneta(String matricula, String marca, String model, double preuBase, double capacitatCarga, Motor motor, Roda[] rodes) {
         super(matricula, marca, model, preuBase, motor, rodes);
         this.capacitatCarga = capacitatCarga;
     }
--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -4,7 +4,7 @@ public class Moto extends Vehicle {
 
     private double cilindrada;
 
-    public Moto(String matricula, String marca, String model, double preuBase, double cilindrada, Motor motor, Roda rodes, String etiquetaAmbiental) {
+    public Moto(String matricula, String marca, String model, double preuBase, double cilindrada, Motor motor, Roda[] rodes, String etiquetaAmbiental) {
         super(matricula, marca, model, preuBase, motor, rodes);
     }
 
