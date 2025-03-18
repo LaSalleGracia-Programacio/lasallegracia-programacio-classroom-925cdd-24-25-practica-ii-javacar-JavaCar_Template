7c6da769057558203b3dd8b4e5f6e65b86ec17e7
oriol <uriolcorbella06@gmail.com>
2025-03-18 10:43:11 +0100

    en el cas que coincideixin en usuari i contrasenya recorrem el for i retorna true, si no coincideix false

--- src/main/java/org/JavaCar/Main.java
+++ src/main/java/org/JavaCar/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Benvingut a CarManager");
+        Scanner input = new Scanner(System.in);
     }
 
     public class sistemaLogin{
         static String[][] usuaris = new String[100][2];
         static int numUsuaris = 0;
-        Scanner input = new Scanner(System.in);
 
         public static void registrarUsuari(){
             Scanner input = new Scanner(System.in);
@@ -29,6 +29,17 @@ public class Main {
             String nomUsuari = input.nextLine();
             System.out.println("Introdueix la contrasenya: ");
             String contrasenya = input.nextLine();
+
+            for (int i = 0; i < usuaris.length; i++) {
+                //en el cas que l'usuari coincideixi (retornem true)
+                if (usuaris[i][0].equals(nomUsuari) && usuaris[i][1].equals(contrasenya)) {
+                    return true;
+                }
+            }
+                //en el cas que l'usuari no coincideixi (retornem false)
+            System.out.println("L'Usuari o la contrasenya no coincideixen");
+            return false;
         }
     }
+
 }
