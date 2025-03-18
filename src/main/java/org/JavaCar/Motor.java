f6cb956bb612ac21b29a4dd132eb06d20dbeeabe
Pablo <pablo.arenas@gracia.lasalle.cat>
2025-03-18 12:48:04 +0100

    Creacion de la clase motor, debido a errores con el merge, voy a crear las clases que faltan para hacer posible el merge y que luego raul las sobreescriba con las suyas que son las que esten bien

---
+++ src/main/java/org/JavaCar/Motor.java
@@ -0,0 +1,10 @@
+package org.JavaCar;
+
+public class Motor {
+    private String tipus;  // Tipo de motor (Gasolina, Diesel, etc.)
+    private int potencia;  // Potencia del motor
+
+    public Motor(String tipus, int potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+    }
