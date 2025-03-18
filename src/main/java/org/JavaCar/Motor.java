a075b08191bfa7bb9889503c9e91501849adecf7
Pablo <pablo.arenas@gracia.lasalle.cat>
2025-03-18 12:48:47 +0100

    Finalizacion de la clase motor

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -8,3 +8,11 @@ public class Motor {
         this.tipus = tipus;
         this.potencia = potencia;
     }
+    public String getTipus() {
+        return tipus;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+}
