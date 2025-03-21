4b17a186915c5e26e1202e9bdb1e25826b28cb5d
perez <raul.perez@gracia.lasalle.cat>
2025-03-21 12:44:31 +0100

    creo el constructor de los atributos

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -10,4 +10,13 @@ public abstract class Vehicle implements Llogable {
     protected Roda[] rodes; // Diàmetre de les rodes (totes iguals).
     protected EtiquetaAmbiental etiquetaAmbiental;
  //constructor
+    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, EtiquetaAmbiental etiquetaAmbiental) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+        this.etiquetaAmbiental = etiquetaAmbiental;
+    }
 }
