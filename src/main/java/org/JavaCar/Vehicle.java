7014c946871e109eff560806d60edf5e264b1c49
Josep <josep-andreu.lizana@gracia.lasalle.cat>
2025-03-21 12:45:41 +0100

    he creado los atributos y el constructor

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,6 +1,26 @@
 package org.JavaCar;
 
+import java.util.ArrayList;
+
 abstract class Vehicle implements Llogable {
+    protected String matricula;
+    protected String marca;
+    protected String modelo;
+    protected List<Roda> rodes;
+    protected Motor motor;
+    protected String tipusCombustible;
+    protected String etiquetaAmbiental;
+
+    public Vehicle(String matricula, String marca, String model, Motor motor, String tipusCombustible, String etiquetaAmbiental) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.rodes = new ArrayList<>();
+        this.motor = motor;
+        this.tipusCombustible = tipusCombustible;
+        this.etiquetaAmbiental = etiquetaAmbiental;
+
+    }
 
 
 }
