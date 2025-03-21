cd7903ac3275e082d235cc6d52e7fa7d97c596cc
perez <raul.perez@gracia.lasalle.cat>
2025-03-21 12:20:37 +0100

    Creo el constructor de marca y diametre

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -3,4 +3,9 @@ package org.JavaCar;
 public class Roda {
     private String marca;
     private double diametre;
+
+    public Roda(String marca, double diametre) {
+        this.marca = marca;
+        this.diametre = diametre;
+    }
 }
