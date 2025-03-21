dfc684e2b0fa5052f75a8bffa0a4e92d85d455ac
perez <raul.perez@gracia.lasalle.cat>
2025-03-21 12:43:58 +0100

    pongo la clase en abstract y que implenta de llogable y declaro sus atributos en protected para que solo puedan acceder las subclasses

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,4 +1,13 @@
 package org.JavaCar;
 
-public class Vehicle {
+public abstract class Vehicle implements Llogable {
+    // Atributs (protected per accés des de subclasses)
+    protected String matricula;
+    protected String marca;
+    protected String model;
+    protected double preuBase;
+    protected Motor motor;
+    protected Roda[] rodes; // Diàmetre de les rodes (totes iguals).
+    protected EtiquetaAmbiental etiquetaAmbiental;
+ //constructor
 }
