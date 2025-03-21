f6f67c2af467ecc94d90343a7f38418b6d9302f3
Andreu <andreu.riba@gracia.lasalle.cat>
2025-03-21 12:46:23 +0100

    override furgoneta

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -11,6 +11,26 @@ public class Furgoneta extends Vehicle {
     public int getCapacitatCarga() {
         return capacitatCarga;
     }
+    @Override
+    public String toString() {
+        return "Marca: " + marca + " | " +
+                "Model: " + model + "\n" +
+                "Matricula: " + matricula + " | " +
+                "Any de matriculació: " + anyMatriculacio + "\n" +
+                "Preu/dia: " + preuBase + " | " +
+                "Capacitat de càrrega: " + capacitatCarga + "kg\n" +
+                "Tipus de motor: " + motor.getTipus() + " | " +
+                "Marca de roda: " + rodes.get(0).getMarca() + "\n";
+    }
+
+    @Override
+    public double calcularPreu(int dies) {
+        double preu = preuBase * dies;
+        if (capacitatCarga > 1000) {
+            preu += (preuBase * 0.10 * dies); // Extra del 10%
+        }
+        return preu;
+    }
 
 
 }
