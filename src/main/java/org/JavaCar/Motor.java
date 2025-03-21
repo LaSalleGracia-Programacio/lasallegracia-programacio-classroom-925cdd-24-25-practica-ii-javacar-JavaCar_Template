f6b706166e820afcd2060968de72f893f5b6e1ab
perez <raul.perez@gracia.lasalle.cat>
2025-03-21 12:15:32 +0100

    Creo el constructor de los atributos tipus y potencia

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -3,4 +3,10 @@ package org.JavaCar;
 public class Motor {
     private String tipus;
     private int potencia;
+
+    public Motor(String tipus, int potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+    }
 }
+
