bd665ad6960c3ab081edb933afd84d47274adac0
Josep <josep-andreu.lizana@gracia.lasalle.cat>
2025-03-24 11:36:55 +0100

    he creado un metodo para el gestor de lloguer

--- src/main/java/org/JavaCar/LlogableGestor.java
+++ src/main/java/org/JavaCar/LlogableGestor.java
@@ -1,4 +1,14 @@
 package org.JavaCar;
 
+import java.util.List;
+
 public class LlogableGestor {
+    public double calcularIngressosTotals(List<Vehicle> vehicles, int dies) {
+        double total = 0;
+        for (Vehicle vehicle : vehicles) {
+            total += vehicle.calcularPreu(dies);
+        }
+        return total;
+    }
+
 }
