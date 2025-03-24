479f34a780009b330229a273df64e286cc976c4b
Josep <josep-andreu.lizana@gracia.lasalle.cat>
2025-03-24 11:00:46 +0100

    he tenido que cambiar todo

--- src/main/java/org/JavaCar/ContracteLloguer.java
+++ src/main/java/org/JavaCar/ContracteLloguer.java
@@ -1,4 +1,30 @@
 package org.JavaCar;
+class ContracteLloguer {
+    private int idContracte;
+    private Client client;
+    Private Vehicle vehicle;
+    private int diesLloguer;
+    private double preuTotal;
 
-public class ContracteLloguer {
+
+    public ContracteLloguer(int idContracte, Client client, Private vehicle, int diesLloguer, double preuTotal) {
+        this.idContracte = idContracte;
+        this.client = client;
+        this.Vehicle = vehicle;
+        this.diesLloguer = diesLloguer;
+        this.preuTotal = vehicle.calcularPreu(diesLloguer);
+    }
+
+    public int getIdContracte() {
+        return idContracte;
+    }
+    public Client getClient() {
+        return client;
+    }
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+    public double getPreuTotal() {
+        return preuTotal;
+    }
 }
