8c2d30e990c8e41a9ec905bbfb90a52b2e2efc94
Josep <josep-andreu.lizana@gracia.lasalle.cat>
2025-03-24 11:26:50 +0100

    he creado los atributos y constructor

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -1,4 +1,13 @@
 package org.JavaCar;
 
 public class Motor {
+    private String tipus;
+    private int potencia;
+
+    public Motor(String tipus, int potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+    }
+
+
 }
