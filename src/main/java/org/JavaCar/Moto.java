3469e1031f8314ac54a87314e8ba1830e4233dd5
Josep <josep-andreu.lizana@gracia.lasalle.cat>
2025-03-26 13:03:32 +0100

    he creado la clase moto con  la construcion, getter, y metodo

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -1,4 +1,26 @@
 package org.JavaCar;
 
-public class Moto {
+import java.util.List;
+
+class Moto extends Vehicle {
+
+    private int cilindrada;
+
+    public Moto(String matricula, String marca, String model, int anyFabricacio, String tipusCombustible, String etiquetaAmbiental, List<Roda> rodes, Motor motor, int cilindrada) {
+        super(matricula, marca, model, anyFabricacio, tipusCombustible, etiquetaAmbiental, rodes, motor);
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    @Override
+    public double calcularPreu(int dies) {
+        return 20.0*dies;
+    }
+
+
+
+
 }
