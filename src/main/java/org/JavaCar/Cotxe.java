905165677ced5b3857910be39d9ba76a380d7a12
Nacho <nribasbenosa@gmail.com>
2025-03-26 12:26:50 +0100

    cotxe 1

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -1,14 +1,5 @@
 package org.JavaCar;
-import java.util.List;
 
-public class Cotxe extends Vehicle {
-
-        private int nombrePlaces;
-
-        public Cotxe(String matricula, String marca, String model, int anyFabricacio, String tipusCombustible, String etiquetaAmbiental, List<Roda> rodes, Motor motor, int nombrePlaces) {
-            super(matricula, marca, model, anyFabricacio, tipusCombustible, etiquetaAmbiental, rodes, motor);
-            this.nombrePlaces = nombrePlaces;
-        }
 
         public int getNombrePlaces() {
             return nombrePlaces;
