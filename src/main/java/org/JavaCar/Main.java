73036e7c807f2c71fa1c1e7bc0f803b52c1696e8
Diego <diego.garcia@gracia.lasalle.cat>
2025-03-26 22:01:53 +0100

    importacion:Importación de clases para manejar listas y leer entrada del usuario

--- src/main/java/org/JavaCar/Main.java
+++ src/main/java/org/JavaCar/Main.java
@@ -1,4 +1,8 @@
 package org.JavaCar;
+//Importación de clases para manejar listas y leer entrada del usuario
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
