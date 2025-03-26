e8781dc24fad9dd170fb10a614aa459309e54ca7
Xavier Farrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 11:57:28 +0100

    Update Vehicle.java

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -8,18 +8,44 @@ public abstract class Vehicle implements Llogable {
     protected Motor motor;
     protected Roda[] rodes;
     protected EtiquetaMedioAmbiental etiquetaMedioAmbiental;
-    protected boolean disponibilidad = true;
-    protected int anyMatricula;
+    protected tipusVehicle tipus;
 
-    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, int anyMatricula) {
+    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, int any, tipusVehicle tipus) {
         this.matricula = matricula;
         this.marca = marca;
         this.model = model;
         this.preuBase = preuBase;
         this.motor = motor;
         this.rodes = rodes;
-        this.anyMatricula = anyMatricula;
-        this.etiquetaMedioAmbiental = calcularEtiquetaMedioAmbiental();
+        this.tipus = tipus;
+        this.etiquetaMedioAmbiental = determinarEtiqueta(any);
+    }
+
+    public EtiquetaMedioAmbiental getEtiquetaMedioAmbiental() {
+        return etiquetaMedioAmbiental;
+    }
+
+    private EtiquetaMedioAmbiental determinarEtiqueta(int any) {
+        String tipusCombustible = tipus.getTipo(); // Obtener tipo de combustible
+
+        // Lógica para determinar la etiqueta
+        if (tipusCombustible.equalsIgnoreCase("Eléctrico")) {
+            return EtiquetaMedioAmbiental.ETIQUETA_0_EMISIONES;
+        } else if (tipusCombustible.equalsIgnoreCase("Híbrido Gasolina") && any >= 2018) {
+            return EtiquetaMedioAmbiental.ETIQUETA_ECO;
+        } else if (tipusCombustible.equalsIgnoreCase("Gas") || (tipusCombustible.equalsIgnoreCase("Híbrido Diesel") && any < 2018)) {
+            return EtiquetaMedioAmbiental.ETIQUETA_ECO;
+        } else if (tipusCombustible.equalsIgnoreCase("Gasolina") && (any >= 2006)) {
+            return EtiquetaMedioAmbiental.ETIQUETA_C;
+        } else if (tipusCombustible.equalsIgnoreCase("Diesel") && (any >= 2014)) {
+            return EtiquetaMedioAmbiental.ETIQUETA_C;
+        } else if (tipusCombustible.equalsIgnoreCase("Gasolina") && (any >= 2000)) {
+            return EtiquetaMedioAmbiental.ETIQUETA_B;
+        } else if (tipusCombustible.equalsIgnoreCase("Diesel") && (any >= 2005)) {
+            return EtiquetaMedioAmbiental.ETIQUETA_B;
+        } else {
+            return null; // Sin etiqueta
+        }
     }
 
     public String getMatricula() {
@@ -46,59 +72,23 @@ public abstract class Vehicle implements Llogable {
         return rodes;
     }
 
-    public EtiquetaMedioAmbiental getEtiquetaMedioAmbiental() {
-        return etiquetaMedioAmbiental;
-    }
-
-    public void setEtiquetaMedioAmbiental(EtiquetaMedioAmbiental etiquetaMedioAmbiental) {
-        this.etiquetaMedioAmbiental = etiquetaMedioAmbiental;
-    }
-
     public double calcularPreu(int dies) {
         return this.preuBase * dies;
     }
 
     public String printVehicle() {
         String form = "Matricula: " + getMatricula() + "\n" +
-                "Marca: " + getMarca() + "\n" +
-                "Model: " + getModel() + "\n" +
-                "PreuBase: " + getPreuBase() + "\n" +
-                "Tipus(Motor): " + motor.getTipus() + "\n" +
-                "Potencia(Motor): " + motor.getPotencia() + "\n";
+                            "Marca: " + getMarca() + "\n" +
+                            "Model: " + getModel() + "\n" +
+                            "PreuBase: " + getPreuBase() + "\n" +
+                            "Tipus(Motor): " + motor.getTipus() + "\n" +
+                            "Potencia(Motor): " + motor.getPotencia() + "\n" +
+                            "Etiqueta Medio Ambiental: " + (getEtiquetaMedioAmbiental() != null ? getEtiquetaMedioAmbiental().getNombre() : "Sin Etiqueta") + "\n"; // Añadir etiqueta ambiental
+
         for (Roda i : rodes) {
             form += "Roda(marca): " + i.getMarca() + "\nRoda(Diametre): " + i.getDiametre() + "\n";
         }
         System.out.println(form);
         return form;
-    }
-
-    private EtiquetaMedioAmbiental calcularEtiquetaMedioAmbiental() {
-        if (motor.getTipus().equals(tipusVehicle.ELECTRIC.toString())) {
-            return EtiquetaMedioAmbiental.ETIQUETA_0_EMISIONES;
-        } else if (motor.getTipus().equals(tipusVehicle.HIBRID_GASOLINA.toString()) || motor.getTipus().equals(tipusVehicle.HIBRID_DIESEL.toString())) {
-            if (anyMatricula >= 2014) {
-                return EtiquetaMedioAmbiental.ETIQUETA_ECO;
-            } else {
-                return EtiquetaMedioAmbiental.ETIQUETA_C;
-            }
-        } else if (motor.getTipus().equals(tipusVehicle.GASOLINA.toString())) {
-            if (anyMatricula >= 2006) {
-                return EtiquetaMedioAmbiental.ETIQUETA_C;
-            } else if (anyMatricula >= 2001) {
-                return EtiquetaMedioAmbiental.ETIQUETA_B;
-            } else {
-                return null;
-            }
-        } else if (motor.getTipus().equals(tipusVehicle.DIESEL.toString())) {
-            if (anyMatricula >= 2015) {
-                return EtiquetaMedioAmbiental.ETIQUETA_C;
-            } else if (anyMatricula >= 2006) {
-                return EtiquetaMedioAmbiental.ETIQUETA_B;
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
     }
 }
