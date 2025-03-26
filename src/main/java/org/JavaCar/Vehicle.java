393c73736b16d0ed2653d9f1c3f11085efdcfbc7
Xavier Farrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 11:41:28 +0100

    Update Vehicle.java

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -9,14 +9,17 @@ public abstract class Vehicle implements Llogable {
     protected Roda[] rodes;
     protected EtiquetaMedioAmbiental etiquetaMedioAmbiental;
     protected boolean disponibilidad = true;
+    protected int anyMatricula;
 
-    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes) {
+    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, int anyMatricula) {
         this.matricula = matricula;
         this.marca = marca;
         this.model = model;
         this.preuBase = preuBase;
         this.motor = motor;
         this.rodes = rodes;
+        this.anyMatricula = anyMatricula;
+        this.etiquetaMedioAmbiental = calcularEtiquetaMedioAmbiental();
     }
 
     public String getMatricula() {
@@ -67,5 +70,35 @@ public abstract class Vehicle implements Llogable {
         }
         System.out.println(form);
         return form;
+    }
+
+    private EtiquetaMedioAmbiental calcularEtiquetaMedioAmbiental() {
+        if (motor.getTipus().equals(tipusVehicle.ELECTRIC.toString())) {
+            return EtiquetaMedioAmbiental.ETIQUETA_0_EMISIONES;
+        } else if (motor.getTipus().equals(tipusVehicle.HIBRID_GASOLINA.toString()) || motor.getTipus().equals(tipusVehicle.HIBRID_DIESEL.toString())) {
+            if (anyMatricula >= 2014) {
+                return EtiquetaMedioAmbiental.ETIQUETA_ECO;
+            } else {
+                return EtiquetaMedioAmbiental.ETIQUETA_C;
+            }
+        } else if (motor.getTipus().equals(tipusVehicle.GASOLINA.toString())) {
+            if (anyMatricula >= 2006) {
+                return EtiquetaMedioAmbiental.ETIQUETA_C;
+            } else if (anyMatricula >= 2001) {
+                return EtiquetaMedioAmbiental.ETIQUETA_B;
+            } else {
+                return null;
+            }
+        } else if (motor.getTipus().equals(tipusVehicle.DIESEL.toString())) {
+            if (anyMatricula >= 2015) {
+                return EtiquetaMedioAmbiental.ETIQUETA_C;
+            } else if (anyMatricula >= 2006) {
+                return EtiquetaMedioAmbiental.ETIQUETA_B;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 }
