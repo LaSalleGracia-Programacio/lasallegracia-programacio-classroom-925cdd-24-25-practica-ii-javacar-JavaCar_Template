a2dc93671ca75a1c2cb14f89a54d080bfa8c87a2
Xavier Farrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 12:13:49 +0100

    Actualización

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -2,9 +2,9 @@ package org.JavaCar;
 
 public class Roda {
     private String marca;
-    private int diametre;
+    private double diametre;
 
-    public Roda(String marca, int diametre) {
+    public Roda(String marca, double diametre) {
         this.marca = marca;
         this.diametre = diametre;
     }
