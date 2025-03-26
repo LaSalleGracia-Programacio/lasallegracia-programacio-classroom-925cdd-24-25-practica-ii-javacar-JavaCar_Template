d61465341cefc3fe290ab0ee676fbd4888d4ea0d
Xavier Farrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 11:36:08 +0100

    Update Motor.java

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -3,14 +3,14 @@ package org.JavaCar;
 public class Motor {
     private String tipus;
     private int potencia;
-    public Motor(String tipus, int potencia){
+    public Motor(String tipus, int potencia) {
         this.tipus = tipus;
         this.potencia = potencia;
     }
-    public String getTipus(){
+    public String getTipus() {
         return tipus;
     }
-    public int getPotencia(){
+    public int getPotencia() {
         return potencia;
     }
 }
