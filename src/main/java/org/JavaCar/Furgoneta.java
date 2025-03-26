d2ef69b4710ec9a8a0180bafc4e13ea76806d32b
Xavier Farrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 12:08:04 +0100

    Update Furgoneta.java

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -4,7 +4,7 @@ public class Furgoneta extends Vehicle implements Llogable {
     private int capacitatCarga;
 
     public Furgoneta(String Matr, String Marca, String Model, double preu, int Carga, Motor Motor, Roda[] Rodes, int any, tipusVehicle tipus) {
-        super(Matr, Marca, Model, preu, Motor, Rodes, any, tipus); // Llamada al constructor de Vehicle
+        super(Matr, Marca, Model, preu, Motor, Rodes, any, tipus);
         this.capacitatCarga = Carga;
     }
 
@@ -15,7 +15,7 @@ public class Furgoneta extends Vehicle implements Llogable {
     @Override
     public double calcularPreu(int dies) {
         if (capacitatCarga >= 1000) {
-            return (getPreuBase() + 10) * dies; // Precio adicional para furgonetas con alta capacidad
+            return (getPreuBase() + 10) * dies;
         } else {
             return getPreuBase() * dies;
         }
@@ -23,8 +23,8 @@ public class Furgoneta extends Vehicle implements Llogable {
 
     @Override
     public String printVehicle() {
-        String form = super.printVehicle(); // Llamar al método de la clase padre
-        form += "Capacidad de Carga: " + getCapacitatCarga() + "\n"; // Añadir información específica de la furgoneta
+        String form = super.printVehicle();
+        form += "Capacidad de Carga: " + getCapacitatCarga() + "\n";
         return form;
     }
 }
