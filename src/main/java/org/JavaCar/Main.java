a039f5440aa6d2706f68f0f8f9087d245f81cc46
Diego <diego.garcia@gracia.lasalle.cat>
2025-03-26 22:02:13 +0100

    declaracion:Declaración de listas para vehículos, solicitudes y control de ingresos

--- src/main/java/org/JavaCar/Main.java
+++ src/main/java/org/JavaCar/Main.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    //Declaración de listas para vehículos, solicitudes y control de ingresos
+    private static List<Vehicle> vehicles = new ArrayList<>();
+    private static List<Vehicle> vehiclesLlogats = new ArrayList<>();
+    private static Scanner escaner = new Scanner(System.in);
+    private static List<Solicitud> solicitudes = new ArrayList<>();
+    private static double ingressosTotals = 0;
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
