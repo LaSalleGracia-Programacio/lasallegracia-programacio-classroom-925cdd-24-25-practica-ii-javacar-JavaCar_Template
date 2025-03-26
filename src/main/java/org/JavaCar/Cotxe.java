d14e54cf24d109dbd1d16be223ed2162f61cf489
Nacho <nribasbenosa@gmail.com>
2025-03-26 12:27:33 +0100

    cotxe 1

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -1,13 +1,21 @@
 package org.JavaCar;
 
+import java.util.List;
 
         public int getNombrePlaces() {
             return nombrePlaces;
         }
+public class Cotxe {
+    private int nombrePlaces;
 
         @Override
         public double calcularPreu(int dies) {
             return 30.0 * dies; // Corrección del espacio en el operador *
         }
+    public Cotxe(String matricula, String marca, String model, int anyFabricacio,
+                 String tipusCombustible, String etiquetaAmbiental, List<Roda> rodes,
+                 Motor motor, int nombrePlaces) {
+        super(matricula, marca, model, anyFabricacio, tipusCombustible, etiquetaAmbiental, rodes, motor);
+        this.nombrePlaces = nombrePlaces;
     }
 
