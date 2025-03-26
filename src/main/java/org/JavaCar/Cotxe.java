b4dfdd3eab84c9b900a385c649628b6fe0e2bee3
Xavier Farrus <xavier.farrus@gracia.lasalle.cat>
2025-03-26 12:03:27 +0100

    Update Cotxe.java

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -1,16 +1,26 @@
 package org.JavaCar;
 
-public class Cotxe extends Vehicle implements Llogable{
+public class Cotxe extends Vehicle implements Llogable {
     private int nombrePlaces;
-    public Cotxe(String Matr, String Marca, String Model, double preu, int places, Motor Motor,Roda[] Rodes){
-        super(Matr,Marca,Model,preu,Motor,Rodes);
+
+    public Cotxe(String Matr, String Marca, String Model, double preu, int places, Motor Motor, Roda[] Rodes, int any, tipusVehicle tipus) {
+        super(Matr, Marca, Model, preu, Motor, Rodes, any, tipus); // Llamada al constructor de Vehicle
         this.nombrePlaces = places;
     }
-    public int getNombrePlaces(){
+
+    public int getNombrePlaces() {
         return nombrePlaces;
     }
+
     @Override
-    public double calcularPreu(int dies){
-        return getPreuBase()*dies;
+    public double calcularPreu(int dies) {
+        return getPreuBase() * dies; // Cálculo simple para el precio
+    }
+
+    @Override
+    public String printVehicle() {
+        String form = super.printVehicle(); // Llamar al método de la clase padre
+        form += "Número de Plazas: " + getNombrePlaces() + "\n"; // Añadir información específica del coche
+        return form;
     }
 }
